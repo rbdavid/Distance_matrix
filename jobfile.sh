92dbb72fbb2fb94419c7ce68b8b4b6153559074d
rbdavid <russell.davidson@colostate.edu>
2016-05-30 18:02:18 -0600

    added lines to move into directories, submit run the job, and then move back out of that directory

--- jobfile.sh
+++ jobfile.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#SBATCH --job-name=dist_cal.a_apo
+#SBATCH --output=dist_cal.a_apo.output
+#SBATCH --time=96:00:00 
+#SBATCH --nodes=1
+#SBATCH --exclusive
+
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/software/usr/gcc-4.9.2/lib64"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/software/usr/hpcx-v1.2.0-292-gcc-MLNX_OFED_LINUX-2.4-1.0.0-redhat6.6/ompi-mellanox-v1.8/lib"
+
+export PYTHON_EGG_CACHE="./"
 
 PDB_LOC='~/Projects/Molecular_Machines/Helicase_DNS3/Analysis/AMBER_apo/truncated.pdb'
 TRAJ_LOC='~/Projects/Molecular_Machines/Helicase_DNS3/Analysis/AMBER_apo/'
@@ -16,7 +26,9 @@ do
 		printf -v x "%03d" $prod
 		printf -v y "%03d" $a
 		mkdir $x.$y.distance_matrix
+		cd $x.$y.distance_matrix
 		time ./matrix_calc.py $pdb_loc $traj_loc $prod $a &
+		cd ../
 		((j=$j+1))
 		((prod=$prod+5))
 	done
