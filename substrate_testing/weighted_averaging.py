d01147765593cdd91999915d267067db50954c77
rbdavid <russell.davidson@colostate.edu>
2016-09-23 14:48:43 -0600

    fixed some bugs in the substrate-Protein contact map weighted_averaging script

--- substrate_testing/weighted_averaging.py
+++ substrate_testing/weighted_averaging.py
@@ -17,6 +17,7 @@ from plotting_functions import *
 window_file = sys.argv[1]
 selection_file = sys.argv[2]
 nProt_res = int(sys.argv[3])
+system = sys.argv[4]
 
 zeros = np.zeros
 change_dir = os.chdir
@@ -35,7 +36,7 @@ nWindows = len(window_data)
 
 sels = open(selection_file,'r')
 count = 0
-for line in file:
+for line in sels:
 	if not line.startswith('#'): 
 		count += 1
 
@@ -46,8 +47,8 @@ nSteps = 0
 for i in range(nWindows):
 	change_dir('%03d.%03d.distance_matrix' %(window_data[i][0],window_data[i][1]))
 
-	avg_data = np.loadtxt('%03d.%03d.avg_distance_matrix.dat' %(window_data[i][0],window_data[i][1]))
-	std_data = np.loadtxt('%03d.%03d.stdv_distance_matrix.dat' %(window_data[i][0],window_data[i][1]))
+	avg_data = np.loadtxt('%03d.%03d.%s.avg_dist_mtx.dat' %(window_data[i][0],window_data[i][1],system))
+	std_data = np.loadtxt('%03d.%03d.%s.stdv_dist_mtx.dat' %(window_data[i][0],window_data[i][1],system))
 
 	avg_data *= window_data[i][2]
 	std_data = square(std_data)
@@ -69,6 +70,6 @@ with open('%03d.%03d.avg_distance_matrix.dat' %(window_data[0][0],window_data[-1
 with open('%03d.%03d.std_distance_matrix.dat' %(window_data[0][0],window_data[-1][1]),'w') as f:
 	np.savetxt(f,std_array)
 
-matrix2d(avg_array,'Nucleic Selection','Protein Residue Number','Distance','%03d.%03d.pro-ssRNA' %(window_data[0][0],window_data[-1][1]),'avg',vmin=0.001,vmax=45.0.plt_title='Avg COM-COM Distance (Traj %03d - %03d)' %(window_data[0][0],window_data[-1][1]),cb_units='$\AA$')
-matrix2d(avg_array,'Nucleic Selection','Protein Residue Number','Distance','%03d.%03d.pro-ssRNA' %(window_data[0][0],window_data[-1][1]),'std',vmin=0.001,vmax=45.0.plt_title='St. Dev. COM-COM Distance (Traj %03d - %03d)' %(window_data[0][0],window_data[-1][1]),cb_units='$\AA$')
+matrix2d(avg_array,'Nucleic Selection','Protein Residue Number','Distance','%03d.%03d.pro-ssRNA' %(window_data[0][0],window_data[-1][1]),'avg',vmin=0.001,vmax=45.0,plt_title='Avg COM-COM Distance (Traj %03d - %03d)' %(window_data[0][0],window_data[-1][1]),cb_units='$\AA$',ylim=(0,nProt_res),xlim=(0,count))
+matrix2d(std_array,'Nucleic Selection','Protein Residue Number','Distance','%03d.%03d.pro-ssRNA' %(window_data[0][0],window_data[-1][1]),'std',vmin=0.001,vmax=6.0,plt_title='St. Dev. COM-COM Distance (Traj %03d - %03d)' %(window_data[0][0],window_data[-1][1]),cb_units='$\AA$',ylim=(0,nProt_res),xlim=(0,count))
 
